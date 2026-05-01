f34d39fbf7e31872c7269de81a62386df01de9af
Jacob Schaupp <jacob.schaupp@gmail.com>
2026-05-01 16:43:04 -0400

    Additional rename

--- src/fogvis/db/__init__.py
+++ src/fogvis/db/__init__.py
@@ -1,4 +1,4 @@
 from .database import Database
 from .database_writer import DatabaseWriter
 from .entities import CoordinateEntity, SceneEntity, CameraEntity, FogTypeEntity, FogEntity, LightTypeEntity, LightEntity, EnvironmentEntity, EnvironmentLightEntity, ImageEntity
-from .importer_image import ImageImporter
+from .image_importer import ImageImporter
